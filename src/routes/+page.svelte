fab9865358cc098febb8daa79dfc43a95c3f206c
Murilo De Mello Martins (MD) <99810903@ambev.com.br>
2024-08-16 15:16:17 -0300

    Commit do template inicial
    Repo destinado a sur usando como ponto de partida para o desenvolvimento
    de webapps.
    
    usa Svelte-Kit com tailwind e svelte-shadcn para o frontend e já com o
    adpter para deploy na vercel

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { getLocalTimeZone, today } from "@internationalized/date";
+    import { Calendar } from "$lib/components/ui/calendar/index.js";
+
+    let value = today(getLocalTimeZone());
+  </script>
+   
+  <Button>Button</Button>
+
+  <Calendar bind:value class="rounded-md border shadow" />
